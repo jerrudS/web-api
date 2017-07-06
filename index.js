7c23352c17304b5049cee07368e689bca49c3014
Jerrud Sabourin <jsabourin1235@gmail.com>
2017-07-06 10:33:53 -0700

    Register a route for POST requests to /notes that adds each request body to /notes and responds with a 201 "Created" status code. #1

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
 
 const notes = []
+let newId = 0
+
+app.use(bodyParser.json())
 
 app.get('/notes', (req, res) => {
   res.json(notes)
+})
+
+app.post('/notes', (req, res) => {
+  newId += 1
+  const note = req.body
+  note.id = newId
+  notes.push(req.body)
+  res.sendStatus(201)
 })
 
 app.listen(3000, () => {
